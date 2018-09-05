04e928d878ae0c5546dacb4738a2899ab4332d08
Ben Keen <ben.keen@gmail.com>
2018-09-04 21:44:03 -0700

    rollup now rolling up

---
+++ example/README.md
@@ -0,0 +1,1 @@
+### react-country-region-selector example
---
+++ example/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CountryDropdown, RegionDropdown } from '../../dist/index.es';
+import Examples from './Examples';
+
+ReactDOM.render(<Examples />, document.getElementById('examples'));
