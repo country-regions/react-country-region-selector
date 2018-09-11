6aece807832276b94ad25fa234c3309c3f6b4b8f
Ben Keen <ben.keen@gmail.com>
2018-09-10 18:47:07 -0700

    additional tests

--- src/tests/CountryDropdown.test.js
+++ src/tests/CountryDropdown.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { CountryDropdown, CountryRegionData } from '../../dist/rcrs.es';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 
-const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -16,13 +15,6 @@ describe('CountryDropdown', () => {
 		expect(wrapper.find('#fake-id-attribute').length).toBe(0);
 	});
 
-	it('sets name attribute', () => {
-		const wrapper = shallow(
-			<CountryDropdown name="name-attribute" />
-		);
-		expect(wrapper.find('select[name="name-attribute"]').length).toBe(1);
-		expect(wrapper.find('select[name="fake-name-attribute"]').length).toBe(0);
-	});
 
 	it('classes attribute gets recognized', () => {
 		const wrapper = shallow(
@@ -31,7 +23,30 @@ describe('CountryDropdown', () => {
 		expect(wrapper.find('select').hasClass('one two three')).toBe(true);
 	});
 
-	describe('default label', () => {
+	it('passes arbitrary properties', () => {
+		const wrapper = shallow(
+			<CountryDropdown style={{ color: 'red' }} data-whatever="5" />
+		);
+		expect(wrapper.find('select').getElement().props.style.color).toBe('red');
+		expect(wrapper.find('select').getElement().props['data-whatever']).toBe('5');
+	});
+
+	describe('name attribute', () => {
+		it('falls back on default name attribute when not specified', () => {
+			const wrapper = shallow(<CountryDropdown />);
+			expect(wrapper.find('select').getElement().props.name).toBe('rcrs-country');
+		});
+
+		it('sets explicit name attribute', () => {
+			const wrapper = shallow(
+				<CountryDropdown name="name-attribute" />
+			);
+			expect(wrapper.find('select[name="name-attribute"]').length).toBe(1);
+			expect(wrapper.find('select[name="fake-name-attribute"]').length).toBe(0);
+		});
+	});
+
+	describe('disabled attribute', () => {
 		it('disabled attribute not on by default', () => {
 			const wrapper = shallow(
 				<CountryDropdown />
@@ -46,7 +61,7 @@ describe('CountryDropdown', () => {
 		});
 	});
 
-	describe('default label', () => {
+	describe('default blank option', () => {
 		it('showDefaultOption = false removes the default option', () => {
 			const wrapper = shallow(<CountryDropdown showDefaultOption={false} />);
 			expect(wrapper.find('option').length).toBe(CountryRegionData.length);
@@ -65,6 +80,69 @@ describe('CountryDropdown', () => {
 				<CountryDropdown defaultOptionLabel={customLabel} />
 			);
 			expect(wrapper.find('select').childAt(0).text()).toBe(customLabel);
+		});
+	});
+
+	describe('country list', () => {
+		it('outputs the list of countries', () => {
+			const wrapper = shallow(<CountryDropdown />);
+			expect(wrapper.find('option').length).toBe(CountryRegionData.length + 1); // 1 for the "Select Country" default option
+		});
+
+		it('respects the blacklist', () => {
+			const blacklist = ['GB', 'CA', 'US'];
+			const wrapper = shallow(
+				<CountryDropdown blacklist={blacklist} showDefaultOption={false} />
+			);
+			expect(wrapper.find('option').length).toBe(CountryRegionData.length - blacklist.length);
+
+			// confirm a non-blacklist item appears
+			expect(wrapper.find('option[value="Afghanistan"]').length).toBe(1);
+
+			// confirm none of the blacklist item appears
+			expect(wrapper.find('option[value="United Kingdom"]').length).toBe(0);
+			expect(wrapper.find('option[value="Canada"]').length).toBe(0);
+			expect(wrapper.find('option[value="United States"]').length).toBe(0);
+		});
+
+		it('respects the whitelist', () => {
+			const whitelist = ['GB', 'CA', 'US'];
+			const wrapper = shallow(
+				<CountryDropdown whitelist={whitelist} showDefaultOption={false} />
+			);
+			expect(wrapper.find('option').length).toBe(whitelist.length);
+
+			// confirm the expected items appear
+			expect(wrapper.find('option[value="United Kingdom"]').length).toBe(1);
+			expect(wrapper.find('option[value="Canada"]').length).toBe(1);
+			expect(wrapper.find('option[value="United States"]').length).toBe(1);
+		});
+	});
+
+	describe('valueType', () => {
+		it('confirm value is full country name by default', () => {
+			const wrapper = shallow(
+				<CountryDropdown showDefaultOption={false} />
+			);
+			expect(wrapper.find('select').childAt(0).getElement().props.value).toBe(CountryRegionData[0][0]);
+		});
+
+		it('confirm explicit valueType="full" also sets full country name', () => {
+			const wrapper = shallow(
+				<CountryDropdown
+					showDefaultOption={false}
+					valueType="full" />
+			);
+			expect(wrapper.find('select').childAt(0).getElement().props.value).toBe(CountryRegionData[0][0]);
+		});
+
+		it('confirm valueType="short" outputs country short code', () => {
+			const wrapper = shallow(
+				<CountryDropdown
+					showDefaultOption={false}
+					valueType="short" />
+			);
+			expect(wrapper.find('select').childAt(0).getElement().props.value).toBe(CountryRegionData[0][1]);
 		});
 	});
 
@@ -91,53 +169,4 @@ describe('CountryDropdown', () => {
 		});
 	});
 
-	describe('country list', () => {
-		it('outputs the list of countries', () => {
-			const wrapper = shallow(<CountryDropdown />);
-			expect(wrapper.find('option').length).toBe(CountryRegionData.length + 1); // 1 for the "please select" default option
-		});
-
-		it('respects the blacklist', () => {
-			const blacklist = ['GB', 'CA', 'US'];
-			const wrapper = shallow(
-				<CountryDropdown blacklist={blacklist} showDefaultOption={false} />
-			);
-			expect(wrapper.find('option').length).toBe(CountryRegionData.length - blacklist.length);
-		});
-
-		it('respects the whitelist', () => {
-			const whitelist = ['GB', 'CA', 'US'];
-			const wrapper = shallow(
-				<CountryDropdown whitelist={whitelist} showDefaultOption={false} />
-			);
-			expect(wrapper.find('option').length).toBe(whitelist.length);
-		});
-	});
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ src/tests/RegionDropdown.test.js
@@ -0,0 +1,181 @@
+import React from 'react';
+import { RegionDropdown } from '../../dist/rcrs.es';
+import Enzyme, { shallow } from 'enzyme';
+
+const Adapter = require('enzyme-adapter-react-16');
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('RegionDropdown', () => {
+
+	it('sets ID attribute', () => {
+		const wrapper = shallow(
+			<RegionDropdown id="id-attribute" />
+		);
+		expect(wrapper.find('#id-attribute').length).toBe(1);
+		expect(wrapper.find('#fake-id-attribute').length).toBe(0);
+	});
+
+	it('classes attribute gets recognized', () => {
+		const wrapper = shallow(
+			<RegionDropdown classes="one two three" />
+		);
+		expect(wrapper.find('select').hasClass('one two three')).toBe(true);
+	});
+
+	describe('name attribute', () => {
+		it('falls back on default name attribute when not specified', () => {
+			const wrapper = shallow(<RegionDropdown />);
+			expect(wrapper.find('select').getElement().props.name).toBe('rcrs-region');
+		});
+
+		it('sets explicit name attribute', () => {
+			const wrapper = shallow(
+				<RegionDropdown name="name-attribute" />
+			);
+			expect(wrapper.find('select[name="name-attribute"]').length).toBe(1);
+			expect(wrapper.find('select[name="fake-name-attribute"]').length).toBe(0);
+		});
+	});
+
+	describe('disabled attribute', () => {
+		it('not disabled by default', () => {
+			const wrapper = shallow(
+				<RegionDropdown />
+			);
+			expect(wrapper.find('select').getElement().props.disabled).toBe(false);
+		});
+		it('disabled attribute', () => {
+			const wrapper = shallow(
+				<RegionDropdown disabled={true} />
+			);
+			expect(wrapper.find('select').getElement().props.disabled).toBe(true);
+		});
+	});
+
+	describe('default option', () => {
+		it('confirm default label when there are no countries is "-"', () => {
+			const wrapper = shallow(<RegionDropdown />);
+			expect(wrapper.find('select').childAt(0).text()).toBe('-');
+		});
+
+		it('confirm default label when there are countries is "Select Region"', () => {
+			const wrapper = shallow(<RegionDropdown country="Canada" />);
+			expect(wrapper.find('select').childAt(0).text()).toBe('Select Region');
+		});
+
+		it('defaultOptionLabel - applies only when a country is passed note!', () => {
+			const customLabel = 'Holy moly I am a custom label!';
+			const wrapper = shallow(
+				<RegionDropdown defaultOptionLabel={customLabel} country="Canada" />
+			);
+			expect(wrapper.find('select').childAt(0).text()).toBe(customLabel);
+		});
+	});
+
+	describe('country parameter', () => {
+
+		describe('blank country parameter', () => {
+			it('setting no country parameter shows the default blank option and no other options', () => {
+				const wrapper = shallow(
+					<RegionDropdown country="" />
+				);
+				expect(wrapper.find('option').length).toBe(1);
+				expect(wrapper.find('option').text()).toBe('-');
+			});
+
+			it('does not disable the region field by default when there is no country param', () => {
+				const wrapper = shallow(
+					<RegionDropdown country="" />
+				);
+				expect(wrapper.find('select').getElement().props.disabled).toBe(false);
+			});
+
+			it('disables the region field when there is no country param and disableWhenEmpty set to true', () => {
+				const wrapper = shallow(
+					<RegionDropdown country="" disableWhenEmpty={true} />
+				);
+				expect(wrapper.find('select').getElement().props.disabled).toBe(true);
+			});
+		});
+
+		describe('with country parameter set', () => {
+			it('shows the regions for the selected country', () => {
+				const wrapper = shallow(
+					<RegionDropdown country="Canada" />
+				);
+				expect(wrapper.find('option').length).toBe(14);
+				expect(wrapper.find('select').childAt(1).text()).toBe('Alberta');
+				expect(wrapper.find('select').childAt(2).text()).toBe('British Columbia');
+			});
+
+			it('does not show any country if you pass an invalid country name', () => {
+				// suppress console.error here
+				const wrapper = shallow(
+					<RegionDropdown country="Chickenland" />
+				);
+				expect(wrapper.find('option').length).toBe(1);
+				expect(wrapper.find('option').text()).toBe('Select Region');
+			});
+		});
+
+		describe('valueType', () => {
+			it('region values are full region name by default', () => {
+				const wrapper = shallow(
+					<RegionDropdown country="Canada" showDefaultOption={false} />
+				);
+				expect(wrapper.find('select').childAt(0).getElement().props.value).toBe('Alberta');
+			});
+
+			it('valueType = full still uses full region name as option values', () => {
+				const wrapper = shallow(
+					<RegionDropdown
+						country="Canada"
+						showDefaultOption={false}
+						valueType="full" />
+				);
+				expect(wrapper.find('select').childAt(0).getElement().props.value).toBe('Alberta');
+			});
+
+			it('valueType changes value to region short codes when specified', () => {
+				const wrapper = shallow(
+					<RegionDropdown
+						country="Canada"
+						showDefaultOption={false}
+						valueType="short" />
+				);
+				expect(wrapper.find('select').childAt(0).getElement().props.value).toBe('AB');
+			});
+		});
+
+		describe('labelType', () => {
+			it('region labels are full region name by default', () => {
+				const wrapper = shallow(
+					<RegionDropdown country="Canada" showDefaultOption={false} />
+				);
+				expect(wrapper.find('select').childAt(0).text()).toBe('Alberta');
+			});
+
+			it('labelType = full still uses full region name as option values', () => {
+				const wrapper = shallow(
+					<RegionDropdown
+						country="Canada"
+						showDefaultOption={false}
+						labelType="full" />
+				);
+				expect(wrapper.find('select').childAt(0).text()).toBe('Alberta');
+			});
+
+			it('valueType changes value to region short codes when specified', () => {
+				const wrapper = shallow(
+					<RegionDropdown
+						country="Canada"
+						showDefaultOption={false}
+						labelType="short" />
+				);
+				expect(wrapper.find('select').childAt(0).text()).toBe('AB');
+			});
+		});
+
+	});
+
+});
