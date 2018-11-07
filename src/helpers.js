189bd79135dc89fc7d470212a885adf3e5fee048
Ben Keen <ben.keen@gmail.com>
2018-11-06 21:27:43 -0800

    sync

--- src/helpers.js
+++ src/helpers.js
@@ -1,12 +1,19 @@
 // reduces the subset of countries depending on whether the user specified a white/blacklist
 export const filterCountries = (countries, priorityCountries, whitelist, blacklist) => {
 	let filteredCountries = countries;
+	//let priorityList = [];
 
 	if (whitelist.length > 0) {
 		filteredCountries = countries.filter(([, countrySlug]) => whitelist.indexOf(countrySlug) > -1);
 	} else if (blacklist.length > 0) {
 		filteredCountries = countries.filter(([, countrySlug]) => blacklist.indexOf(countrySlug) === -1);
 	}
+
+	// if (priorityCountries.length > 0) {
+	// 	priorityList = filteredCountries.filter(([, countrySlug]) => { return mainOptions.indexOf(countrySlug) > -1; });
+	// 	filteredCountries = filteredCountries.filter(([, countrySlug]) => { return mainOptions.indexOf(countrySlug) === -1; });
+	// }
+	// return mainOptions.length ? [...mainList, ...filteredCountries] : filteredCountries;
 
 	return filteredCountries;
 };
