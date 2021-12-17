d3167ac3c7065e8ef712c97d3fd5adbeff36b9d1
Dmytro Shvaika <shvajka@gmail.com>
2021-12-17 12:40:31 +0200

    typo

--- src/CountryDropdown.js
+++ src/CountryDropdown.js
@@ -43,7 +43,7 @@ export default class CountryDropdown extends Component {
 			name,
 			value,
 			onChange: (e) => onChange(e.target.value, e),
-			onBlur: (e) => onBlur(e),
+			onBlur: (e) => onBlur(e.target.value, e),
 			disabled
 		};
 		if (id) {
