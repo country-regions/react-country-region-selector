b0782badd8f251984c93081b7921cc4b0927ca3b
Dmytro Shvaika <shvajka@gmail.com>
2021-12-17 12:53:57 +0200

    format countries alphabetically

--- format-countries.js
+++ format-countries.js
@@ -3,5 +3,13 @@ const fs = require('fs');
 
 fs.writeFileSync(
 	'./countries-formatted.json',
-	JSON.stringify(countries.map(({ name: { common: countryName }, cca2: countrySlug }) => [countryName, countrySlug]))
+	JSON.stringify(
+		countries
+			.map(({ name: { common: countryName }, cca2: countrySlug }) => [countryName, countrySlug])
+			.sort(([a], [b]) => {
+				if (a < b) { return -1; }
+				if (a > b) { return 1; }
+				return 0;
+			})
+	)
 );
